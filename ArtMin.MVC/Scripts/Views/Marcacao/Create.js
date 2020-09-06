f4a3327284557544117db26f725c6335ebee6663
Kaio Moscou Chalup <kaio.chalup@gmail.com>
2020-09-06 14:27:40 -0300

    Ajustes na function referente à busca de jogador.

--- ArtMin.MVC/Scripts/Views/Marcacao/Create.js
+++ ArtMin.MVC/Scripts/Views/Marcacao/Create.js
@@ -1,13 +1,14 @@
 ﻿$(document).ready(function () {
     $.ajax({
         type: "GET",
-        url: "https://localhost:44363/Jogador/GetAll", dataType: "json", contentType: "application/json",
+        url: window.caminhoWebSite + "Jogador/GetAll",
+        dataType: "json",
+        contentType: "application/json",
         success: function (res) {
             $.each(res, function (data, value) {
 
                 $("#JogadorId").append($("<option></option>").val(value.JogadorId).html(value.Nome));
             })
         }
-
     });
 })
