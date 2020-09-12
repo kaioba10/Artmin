c0be921605e09ac3e54b7cc3c2e09d17947c1955
Kaio Moscou Chalup <kaio.chalup@gmail.com>
2020-09-12 13:43:20 -0300

    Index.js (Marcação) - Ajuste de idioma - DataTables

--- ArtMin.MVC/Scripts/Views/Marcacao/Index.js
+++ ArtMin.MVC/Scripts/Views/Marcacao/Index.js
@@ -1,5 +1,18 @@
 ﻿$(document).ready(function () {
     $('#tableMarcacoes').DataTable({
+        language: {
+            search: "Pesquisar",
+            sProcessing: "Processando...",
+            sLengthMenu: "Mostrar _MENU_ registros",
+            sZeroRecords: "Nenhum resultado encontrado",
+            sInfoEmpty: "0 resultados",
+            sInfo: "Mostrando de _START_ a _END_ de um total de _TOTAL_ registros",
+            sInfoFiltered: "(Filtrado de um total de _MAX_ registros)",
+            paginate: {
+                "previous": "Página Anterior",
+                "next": "Próxima Página"
+            },
+        },
         "ajax": {
             "url": "Marcacao/ObterMarcacoes",
             "type": "POST",
