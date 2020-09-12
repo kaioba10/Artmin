b769e83c84ff3d2adcbee573ad3c18ce8e511e3c
Kaio Moscou Chalup <kaio.chalup@gmail.com>
2020-09-12 14:04:32 -0300

    Index.js (Marcação) - Melhoria na configuração de idioma.

--- ArtMin.MVC/Scripts/Views/Marcacao/Index.js
+++ ArtMin.MVC/Scripts/Views/Marcacao/Index.js
@@ -1,17 +1,7 @@
 ﻿$(document).ready(function () {
     $('#tableMarcacoes').DataTable({
         language: {
-            search: "Pesquisar",
-            sProcessing: "Processando...",
-            sLengthMenu: "Mostrar _MENU_ registros",
-            sZeroRecords: "Nenhum resultado encontrado",
-            sInfoEmpty: "0 resultados",
-            sInfo: "Mostrando de _START_ a _END_ de um total de _TOTAL_ registros",
-            sInfoFiltered: "(Filtrado de um total de _MAX_ registros)",
-            paginate: {
-                "previous": "Página Anterior",
-                "next": "Próxima Página"
-            },
+            url: '//cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese-Brasil.json'
         },
         "ajax": {
             "url": "Marcacao/ObterMarcacoes",
