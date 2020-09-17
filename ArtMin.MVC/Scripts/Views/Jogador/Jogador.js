635fc5ad4b28e3410b7612adb341e50149287ad0
Kaio Moscou Chalup <kaio.chalup@gmail.com>
2020-09-17 19:29:39 -0300

    Jogador.js - Criação da function "SalvarCadastro()"; - Criação da function "ValidarFormularioCreate()".
    Index.js (Marcação)
    - Ajuste na url da function referente a apresentação do dataTables de marcações cadastradas.
    
    Create.cshtml (Jogador)
    - Incluído id no @Html.BeginForm.
    
    _ModalConfirmacao.cshtml
    - Inclusão de Id no button referente a salvar o formulário.
    
    _Layout.cshtml
    - Inclusão de scripts src e link rel para referenciar frameworks JQuery.

--- ArtMin.MVC/Scripts/Views/Jogador/Jogador.js
+++ ArtMin.MVC/Scripts/Views/Jogador/Jogador.js
@@ -5,8 +5,73 @@
         keepStatic: true
     });
 
+    ValidarFormularioCreate();
+
 });
 
 function ConfirmarCadastro() {
     $('#ConfimacaoCadastro').modal('show');
+}
+
+function SalvarCadastro() {
+
+    var form = $('#formCadastroJogador');
+
+    if (!form.valid()) {
+        $(form).validate();
+        MensagemToastr(tipoToastr.erro, "Favor preencher campos obrigatórios.");
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: caminhoWebSite + "Jogador/CadastrarJogador",
+        dataType: "json",
+        contentType: "application/json",
+        success: function (result) {
+
+            if (result.Sucesso) {
+                MensagemToastr(tipoToastr.sucesso, "Jogador cadastrado com sucesso");
+            }
+            else {
+                MensagemToastr(tipoToastr.erro, "Erro ao cadastrar o jogador");
+                return;
+            }
+        }
+    });
+}
+
+function ValidarFormularioCreate(){
+    $(function () {
+        $("#formCadastroJogador").validate({
+            rules: {
+                Nome: {
+                    required: true,
+                    maxlength: 50,
+                    minlength: 2
+                },
+                Email: {
+                    required: true,
+                    email: true,
+                    maxlength: 120,
+                    minlength: 2
+                },
+                Cpf: "required"
+            },
+            // Mensagens referentes às especificações de regras acima
+            messages: {
+                Nome: {
+                    required: "O campo Nome é obrigatório.",
+                    maxlength: "Permitido no máximo 50 caracteres",
+                    minlength: "Permitido no mínimo 2 caracteres"
+                },
+                Email: "Formato de e-mail inválido.",
+                Cpf: "O campo CPF é obrigatório"
+            },
+
+            submitHandler: function (form) {
+                form.submit();
+            }
+        });
+    });
 }
